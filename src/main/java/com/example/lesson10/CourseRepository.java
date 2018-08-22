80eb418806f8c9b3d5bf4ff92a645f66660257da
Dave Wolf <dave45678@gmail.com>
2018-08-22 16:39:31 -0400

    updated searchform and results

--- src/main/java/com/example/lesson10/CourseRepository.java
+++ src/main/java/com/example/lesson10/CourseRepository.java
@@ -2,5 +2,12 @@ package com.example.lesson10;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface CourseRepository extends CrudRepository<Course, Long> {
+    Course findByTitle(String title);
+    ArrayList<Course> findAllByCredit(int credit);
+    ArrayList<Course> findAllByTitleContainingIgnoreCase(String search);
+    ArrayList<Course> findAllByCreditBetween(int min, int max);
+    Long countAllByInstructor(String instructor);
 }
--- src/main/java/com/example/lesson10/HomeController.java
+++ src/main/java/com/example/lesson10/HomeController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,6 +18,26 @@ public class HomeController {
 
   @RequestMapping("/")
   public String listCourses(Model model){
+
+    Course a = new Course();
+    a.setInstructor("Dave Wolf");
+    a.setTitle("Java Programming");
+    a.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in orci auctor, interdum massa vitae, porttitor leo. Nunc pellentesque est id leo venenatis, vel efficitur dui molestie.");
+    a.setCredit(3);
+    courseRepository.save(a);
+    Course b = new Course();
+    b.setInstructor("Dave Wolf");
+    b.setTitle("Python Automation");
+    b.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in orci auctor, interdum massa vitae, porttitor leo. Nunc pellentesque est id leo venenatis, vel efficitur dui molestie.");
+    b.setCredit(5);
+    courseRepository.save(b);
+    Course c = new Course();
+    c.setInstructor("Alton Henley");
+    c.setTitle("Python Fundamentals");
+    c.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in orci auctor, interdum massa vitae, porttitor leo. Nunc pellentesque est id leo venenatis, vel efficitur dui molestie.");
+    c.setCredit(4);
+    courseRepository.save(c);
+
     model.addAttribute("courses", courseRepository.findAll());
     return "list";
   }
@@ -39,22 +56,60 @@ public class HomeController {
     courseRepository.save(course);
     return "redirect:/";
   }
+    @RequestMapping("/detail/{id}")
+    public String showCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "show";
+    }
 
-  @RequestMapping("/detail/{id}")
-  public String showCourse(@PathVariable("id") long id, Model model){
-    model.addAttribute("course", courseRepository.findById(id).get());
-    return "show";
-  }
+    @RequestMapping("/update/{id}")
+    public String updateCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "courseform";
+    }
 
-  @RequestMapping("/update/{id}")
-  public String updateCourse(@PathVariable("id") long id, Model model){
-    model.addAttribute("course", courseRepository.findById(id).get());
-    return "courseform";
-  }
+    @RequestMapping("/delete/{id}")
+    public String delCourse(@PathVariable("id") long id){
+        courseRepository.deleteById(id);
+        return "redirect:/";
+    }
 
-  @RequestMapping("/delete/{id}")
-  public String delCourse(@PathVariable("id") long id){
-    courseRepository.deleteById(id);
-    return "redirect:/";
-  }
+    // Search Methods
+    @RequestMapping("/search")
+    public String search(){
+        return "searchform";
+    }
+
+    @PostMapping("/findbytitle")
+    public String findbytitle(@RequestParam("title") String title, Model model){
+        model.addAttribute("course", courseRepository.findByTitle(title));
+        return "show";
+    }
+
+    @PostMapping("/findbytitlecontaining")
+    public String findbytitlecontaining(@RequestParam("partialtitle") String partialtitle, Model model){
+        model.addAttribute("courses", courseRepository.findAllByTitleContainingIgnoreCase(partialtitle));
+        return "list";
+    }
+
+    @PostMapping("/findbycredits")
+    public String findbycredits(@RequestParam("credits") int credits, Model model){
+        model.addAttribute("courses", courseRepository.findAllByCredit(credits));
+        return "list";
+    }
+
+    @PostMapping("/findbycreditsbetween")
+    public String findbycreditsbetween(@RequestParam("credits_min") int min,@RequestParam("credits_max") int max, Model model){
+        model.addAttribute("courses", courseRepository.findAllByCreditBetween(min,max));
+        return "list";
+    }
+
+    @PostMapping("/countbyinstructor")
+    public String countbyinstructor(@RequestParam("instructor") String instructor, Model model){
+        model.addAttribute("instructor",instructor);
+        model.addAttribute("counter", courseRepository.countAllByInstructor(instructor));
+        return "results";
+    }
+
+
 }
